9c41f420ebb47646d14a796ed650c5c9fa08f725
felipenunes04 <felipennunes@hotmail.com>
2017-07-31 19:35:02 -0300

    Adicionando testes para números inválidos

--- src/br/edu/ifpi/converteromanos/ConversorDeNumeroRomano.java
+++ src/br/edu/ifpi/converteromanos/ConversorDeNumeroRomano.java
@@ -11,18 +11,38 @@ public class ConversorDeNumeroRomano {
 	}
 	
 	public int converte(String valor) {
-		String numeros[] = valor.split("");
-		int soma = romanos.get(numeros[0]);
-		for (int i = 1; i < numeros.length; i++) {
-			if(romanos.get(numeros[i]) <= romanos.get(numeros[i-1])){
-				soma+=romanos.get(numeros[i]);
-			}else{
-				soma=romanos.get(numeros[i])-romanos.get(numeros[i-1]);
+		String[] numeros = valor.split("");
+		if(validaRomanos(numeros)){
+			int soma = romanos.get(numeros[0]);
+			for (int i = 1; i < numeros.length; i++) {
+				if(romanos.get(numeros[i]) <= romanos.get(numeros[i-1])){
+					soma+=romanos.get(numeros[i]);
+				}else{
+					soma=romanos.get(numeros[i])-romanos.get(numeros[i-1]);
+				}
 			}
+			return soma;
+		}else{
+			return 0;
 		}
-		return soma;
 	}
 	
+	private boolean validaRomanos(String[] numeros) {
+		int contI=0,contV=0,contX=0,contL=0,contC=0,contD=0,contM=0;
+		for (int i = 0; i < numeros.length; i++) {
+			if(numeros[i].equals("I")) contI++;
+			if(numeros[i].equals("V")) contV++;
+			if(numeros[i].equals("X")) contX++;
+			if(numeros[i].equals("L")) contL++;
+			if(numeros[i].equals("C")) contC++;
+			if(numeros[i].equals("D")) contD++;
+			if(numeros[i].equals("M")) contM++;
+		}
+		if(contI>3 || contV>1 || contX>3 || contL>1 || contC>3 || contD>1 ||
+				contM>3) return false;
+		return true;
+	}
+
 	private void iniciaRomanos(Map romanos) {
 		romanos.put("I",1);
 		romanos.put("V",5);
--- src/br/edu/ifpi/converteromanos/ConversorTest.java
+++ src/br/edu/ifpi/converteromanos/ConversorTest.java
@@ -10,10 +10,14 @@ public class ConversorTest {
 		int num1 = conversor.converte("XV");
 		int num2 = conversor.converte("XXVIII");
 		int num3 = conversor.converte("IV");
+		int num4 = conversor.converte("IIIIV");
+		int num5 = conversor.converte("IIVV");
 		
 		Assert.assertEquals(15, num1);
 		Assert.assertEquals(28, num2);
 		Assert.assertEquals(4, num3);
+		Assert.assertEquals(0, num4);
+		Assert.assertEquals(0, num5);
 
 	}
 }
